bc07affbb0fddd13a0dccce4d3300da442cedadc
MomoCow <momocow.me@gmail.com>
2018-03-03 23:59:43 +0800

    3.0.1

--- dist/auction-timer.user.js
+++ dist/auction-timer.user.js
@@ -2,7 +2,7 @@
 // @name           OGame Redesign (v.6): Auction Timer
 // @author         MomoCow
 // @namespace      https://github.com/momocow
-// @version        3.0.0
+// @version        3.0.1
 // @description    Displays a countdown timer for the Auction in OGame 6.*
 // @include        *.ogame*gameforge.com/game/index.php?page=*
 // @updateURL      https://raw.githubusercontent.com/momocow/OGame-Redesign-v.6-Auction-Timer/master/dist/auction-timer.meta.js
